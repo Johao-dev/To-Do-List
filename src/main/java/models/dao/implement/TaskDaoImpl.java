502a3236564f9b55da336dd83112ca33672dd542
johaoDev <johaobravoespinoza1501@gmail.com>
2024-06-25 05:04:52 -0500

    defining common methods

--- src/main/java/models/dao/implement/TaskDaoImpl.java
+++ src/main/java/models/dao/implement/TaskDaoImpl.java
@@ -30,4 +30,45 @@ public class TaskDaoImpl implements TaskDao {
     public void searchTask(Task task) {
 
     }
+    
+    private Connection getConnection() throws SQLException {
+        this.conn = Conexion.getConexion();
+        this.conn.setAutoCommit(false); 
+        return this.conn;
+    }
+    
+    private void closeSources() {
+        try {
+            if (this.st != null)
+                Conexion.close(st);
+            
+            if (this.rs != null)
+                Conexion.close(rs);
+            
+            if (this.conn != null) {
+                this.conn.setAutoCommit(true);
+                Conexion.close(this.conn);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    private void commit(Connection conn) {
+        try {
+            if (conn != null)
+                conn.commit();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    private void rollback(Connection conn) {
+        try {
+            if (conn != null)
+                conn.rollback();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
 }
